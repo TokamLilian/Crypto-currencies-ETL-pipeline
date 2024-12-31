294bb38155a2943476bd01a853562a781e12b3fb
Tokam Lilian <tokamlilian@gmail.com>
2024-12-30 19:22:23 -0500

    Added stored procedures

---
+++ SQL/Procedures/load_pbi_currencies.sql
@@ -0,0 +1,56 @@
+CREATE PROCEDURE powerbi.loadCurrencies
+AS
+BEGIN
+    DECLARE @productData VARCHAR(MAX)
+
+    SELECT @productData = BulkColumn FROM 
+
+    OPENROWSET(BULK'C:\Users\tokam\Documents\Big Projects\Year 2024\Data\src\Exploit-my-data\register\products.json', SINGLE_BLOB) JSON
+    INSERT 
+
+    INTO powerbi.Currencies(
+        product_id,
+        price,
+        price_percentage_change_24h,
+        volume_24h,
+        volume_percentage_change_24h,
+        base_name,
+        quote_name,
+        status,
+        price_increment,
+        display_name,
+        product_venue,
+        approximate_quote_24h_volume,
+        load_date
+    )
+    SELECT
+        product_id,
+        TRY_CAST(price AS DECIMAL(18, 4)),
+        TRY_CAST(price_percentage_change_24h AS DECIMAL(18, 4)),
+        TRY_CAST(volume_24h AS DECIMAL(18, 4)),
+        TRY_CAST(volume_percentage_change_24h AS DECIMAL(18, 4)),
+        base_name,
+        quote_name,
+        status,
+        TRY_CAST(price_increment AS DECIMAL(18, 4)),
+        display_name,
+        product_venue,
+        TRY_CAST(approximate_quote_24h_volume AS DECIMAL(18, 4)),
+        SYSDATETIME() -- use current date from sys
+
+        FROM OPENJSON(@productData, '$.products') 
+            WITH(
+            product_id VARCHAR(100) '$.product_id',
+            price NVARCHAR(100) '$.price',
+            price_percentage_change_24h NVARCHAR(100) '$.price_percentage_change_24h',
+            volume_24h NVARCHAR(100) '$.volume_24h',
+            volume_percentage_change_24h NVARCHAR(100) '$.volume_percentage_change_24h',
+            base_name VARCHAR(100) '$.base_name',
+            quote_name VARCHAR(100) '$.quote_name',
+            status VARCHAR(100) '$.status',
+            price_increment NVARCHAR(100) '$.price_increment',
+            display_name VARCHAR(100) '$.display_name',
+            product_venue VARCHAR(100) '$.product_venue',
+            approximate_quote_24h_volume NVARCHAR(100) '$.approximate_quote_24h_volume'
+        )
+END;
---
+++ SQL/Procedures/transform_src_historical_transactions.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE stg.transformSrcTransactions
+AS
+BEGIN
+
+    DECLARE @JsonData NVARCHAR(MAX);
+
+    -- Read JSON data from source table
+    SELECT @JsonData = [transaction]
+    FROM src.src_Historical_transactions
+    ORDER BY [load_date] ASC;
+
+    -- Parse JSON array and insert each transaction into the destination table
+    INSERT INTO stg.stg_Historical_transactions(
+        [transaction_id],
+        [transaction_date],
+        [symbol],
+        [amount],
+        [exchange],
+        [price],
+        [side],
+        [sell_side_amount],
+        [fee]
+    )
+    SELECT
+        JSON_VALUE(transactions.value, '$.transaction_id'),
+        TRY_CONVERT(DATETIME2, JSON_VALUE(transactions.value, '$.transaction_date')),
+        JSON_VALUE(transactions.value, '$.symbol'),
+        JSON_VALUE(transactions.value, '$.amount'),
+        JSON_VALUE(transactions.value, '$.exchange'),
+        JSON_VALUE(transactions.value, '$.price'),
+        JSON_VALUE(transactions.value, '$.side'),
+        JSON_VALUE(transactions.value, '$.sell_side_amount'),
+        JSON_VALUE(transactions.value, '$.fee')
+    FROM OPENJSON(@JsonData, '$.transactions') AS transactions
+END;
