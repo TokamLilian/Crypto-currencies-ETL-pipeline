bdfd9b0a0ddac0231c949648cdc1a939af6e2036
Tokam Lilian <tokamlilian@gmail.com>
2024-12-31 12:28:15 -0500

    Removed unwanted code at beginning, use environment reference id instead of environment id and change retyr interval of steps to 1 minute

--- SQL/Generation/createJobFinancialData.sql
+++ SQL/Generation/createJobFinancialData.sql
@@ -1,28 +1,4 @@
-﻿-- Not working (i try to get environment id from SSISDB and use it in job creation)
--- USE SSISDB;
--- GO
-
--- -- Declare variables for SSISDB lookup
--- DECLARE @EnvironmentID INT;
--- DECLARE @EnvironmentName NVARCHAR(255) = N'DEV';
--- DECLARE @FolderName NVARCHAR(255) = N'FinancialData';
-
--- -- Lookup Environment ID from SSISDB
--- SELECT @EnvironmentID = env.environment_id
--- FROM catalog.environments AS env
--- INNER JOIN catalog.folders AS f ON env.folder_id = f.folder_id
--- WHERE env.name = @EnvironmentName AND f.name = @FolderName;
-
--- -- Ensure Environment ID is found
--- IF @EnvironmentID IS NULL
--- BEGIN
---     PRINT 'Error: Environment ID not found.';
---     RETURN;
--- END;
-
--- -- Now switch to msdb and create the job
-
-USE SSISDB;
+﻿USE SSISDB;
 GO
 
 DECLARE @EnvironmentName NVARCHAR(255) = N'DEV';
@@ -116,46 +92,46 @@ EXEC sp_add_job
 GO
 
 DECLARE @JobCommand NVARCHAR(300);
-DECLARE @EnvironmentID INT;
+DECLARE @EnvironmentReferenceID INT;
 DECLARE @ServerName NVARCHAR(128);
-SET @EnvironmentID = 1;
+SET @EnvironmentReferenceID = 1;
 SET @ServerName = N'(local)'; -- Replace "(local)" with your actual SQL Server instance name if needed
 
 -- Add the first SSIS package step
-SET @JobCommand = N'/IS "\SSISDB\FinancialData\FD-SSIS\1_Extract_data.dtsx" /SERVER ' + @ServerName + N' /ENVREFERENCE ' + CAST(@EnvironmentID AS NVARCHAR(10));
+SET @JobCommand = N'/IS "\SSISDB\FinancialData\FD-SSIS\1_Extract_data.dtsx" /SERVER ' + @ServerName + N' /ENVREFERENCE ' + CAST(@EnvironmentReferenceID AS NVARCHAR(10));
 EXEC sp_add_jobstep
     @job_name = N'Financial_data_etl',
     @step_name = N'1_extract_data_from_cb',
     @subsystem = N'SSIS', -- Subsystem type for SSIS package execution
     @command = @JobCommand,
     @retry_attempts = 3,      -- Number of retries on failure
-    @retry_interval = 5,      -- Retry interval in minutes
+    @retry_interval = 1,      -- Retry interval in minutes
     @database_name = N'msdb', -- Default database
     @on_success_action = 3,   -- Go to the next step on success
     @on_fail_action = 2;      -- Quit on failure
 
 -- Add the second SSIS package step
-SET @JobCommand = N'/IS "\SSISDB\FinancialData\FD-SSIS\2_Transform_data.dtsx" /SERVER ' + @ServerName + N' /ENVREFERENCE ' + CAST(@EnvironmentID AS NVARCHAR(10));
+SET @JobCommand = N'/IS "\SSISDB\FinancialData\FD-SSIS\2_Transform_data.dtsx" /SERVER ' + @ServerName + N' /ENVREFERENCE ' + CAST(@EnvironmentReferenceID AS NVARCHAR(10));
 EXEC sp_add_jobstep
     @job_name = N'Financial_data_etl',
     @step_name = N'2_transform_source_table',
     @subsystem = N'SSIS',
     @command = @JobCommand,
     @retry_attempts = 3,
-    @retry_interval = 5,
+    @retry_interval = 1,
     @database_name = N'msdb',
     @on_success_action = 3, -- Proceed to the next step
     @on_fail_action = 2;    -- Quit on failure
 
 -- Add the third SSIS package step
-SET @JobCommand = N'/IS "\SSISDB\FinancialData\FD-SSIS\3_Load_data.dtsx" /SERVER ' + @ServerName + N' /ENVREFERENCE ' + CAST(@EnvironmentID AS NVARCHAR(10));
+SET @JobCommand = N'/IS "\SSISDB\FinancialData\FD-SSIS\3_Load_data.dtsx" /SERVER ' + @ServerName + N' /ENVREFERENCE ' + CAST(@EnvironmentReferenceID AS NVARCHAR(10));
 EXEC sp_add_jobstep
     @job_name = N'Financial_data_etl',
     @step_name = N'3_load_into_final_table',
     @subsystem = N'SSIS',
     @command = @JobCommand,
     @retry_attempts = 3,
-    @retry_interval = 5,
+    @retry_interval = 1,
     @database_name = N'msdb',
     @on_success_action = 1, -- Exit with success
     @on_fail_action = 2;    -- Quit on failure
