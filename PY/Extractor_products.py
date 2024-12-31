92e3eb93aa01e9e4258c433a6a03a4db88d6b655
Tokam Lilian <tokamlilian@gmail.com>
2024-12-31 12:28:59 -0500

    Use new public key and changed population method

--- PY/Extractor_products.py
+++ PY/Extractor_products.py
@@ -1,18 +1,30 @@
 import json
 import ccxt
-COINBASE_PUBLIC_KEY = 'OFxXdj03OA9ZTYQL'
-COINBASE_PRIVATE_KEY = 'uDx9QbB6mAr2FxMayVl7cUE6KPyRuJIP'
+#COINBASE_PUBLIC_KEY = 'OFxXdj03OA9ZTYQL'
+#COINBASE_PRIVATE_KEY = 'uDx9QbB6mAr2FxMayVl7cUE6KPyRuJIP'
 COINBASE_SYMBOLS = ["BTC-USD", "ETH-USD", "ETC-USD", "USDT-USD", "BTC-USDT", "ETH-USDT", "SHIB-USD", "BTC-EUR", "ETH-EUR", "USDT-EUR", "BTC-GBP", "ETH-GBP", "NEAR-USD", "MKR-USD", "NMR-USD", "BCH-USD", "ALGO-USD", "USDC-EUR", "USDT-EUR"]
 
 
 products_dir = "C:\\Users\\tokam\\Documents\\Big Projects\\Year 2024\\Data\\src\\Exploit-my-data\\register\\products.json"
+out_dir = "C:\\Users\\tokam\\Documents\\Big Projects\\Year 2024\\Data\\src\\Exploit-my-data\\register\\out.txt"
 
 
 def public_products():
-    client = ccxt.coinbase({'apiKey': COINBASE_PUBLIC_KEY, 'secret': COINBASE_PRIVATE_KEY})
+    #client = ccxt.coinbase({'apiKey': COINBASE_PUBLIC_KEY, 'secret': COINBASE_PRIVATE_KEY})
     products = {}
     try:
-        products = client.fetch_markets()
+        #products = client.fetch_markets()
+
+        import http.client
+        conn = http.client.HTTPSConnection("api.coinbase.com")
+        payload = ''
+        headers = {
+        'Content-Type': 'application/json'
+        }
+        conn.request("GET", "/api/v3/brokerage/market/products", payload, headers)
+        res = conn.getresponse()
+        data = res.read()
+        return json.loads(data.decode("utf-8"))
     except:
         pass#;raise Exception
     return products
@@ -20,10 +32,12 @@ def public_products():
 
 def populate(products):
     array = []
-    for key in products:
-        if key['info']['product_id'] in COINBASE_SYMBOLS:
-            array.append(key['info'])
-
+    #for key in products:
+    for key in products["products"]:
+        #if key['info']['product_id'] in COINBASE_SYMBOLS:
+        if key['product_id'] in COINBASE_SYMBOLS:
+            #array.append(key['info'])
+            array.append(key)
     return array
 
 
